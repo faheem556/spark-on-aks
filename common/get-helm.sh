bf944b7fa23a76d24b90a8977b2063e9f6482ee8
Faheem Memon <memon.faheem@gmail.com>
2020-03-23 12:39:57 -0400

    Helm path fix

--- common/get-helm.sh
+++ common/get-helm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export HELM_INSTALL_DIR=.
+export HELM_INSTALL_DIR=`pwd`
 export DESIRED_VERSION=v3.1.2
+export PATH=$PATH:`pwd`
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
